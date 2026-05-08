243e22facc46d28d6c9d46e2938d85523304be15
artcool213141-max <artcool213141@gmail.com>
2026-05-08 22:31:58 +0300

    Create clear.py

---
+++ clear.py
@@ -0,0 +1,14 @@
+import telebot
+from telebot import types
+
+# Вставь сюда свой токен
+bot = telebot.TeleBot("ТВОЙ_ТОКЕН_БОТА")
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    # Создаем команду на удаление кнопок
+    markup = types.ReplyKeyboardRemove()
+    bot.send_message(message.chat.id, "Кнопки удалены! Теперь открывай Mini App через кнопку меню.", reply_markup=markup)
+
+print("Бот запущен. Напиши ему /start в Телеге...")
+bot.polling()
